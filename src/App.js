faa40623d5c23b46ebd2e3861a9646f5a609b3c6
PedrooSilvaa <pedrohenriquefdasilva14@gmail.com>
2024-05-22 22:28:05 -0300

    feat: change layout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-        <Dash/>
+        <Search/>
     </div>
   );
 }
--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import logo from '../assets/cloud.png';
+import './Dash.css'
 
 const Dash = () => {
 return (
     <div className='container'>
-        
+        <div className="localCard">
+            <div className="pesquisa">
+                <img src={logo} alt="logo" />
+                <input type="text" placeholder='Buscar local'/>
+            </div>
+        </div>
     </div>
 )
 }
